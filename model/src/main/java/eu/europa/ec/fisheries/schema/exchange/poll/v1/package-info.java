a4fad50657dcbe30e21bb98905ba1d0d4bf93a4e
emlynnjoh <emlynnjoh@mycompany.com>
2015-10-07 07:25:42 +0000

    @Review Johanna, Nhi Svn ignore
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Exchange/APP@5272 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- model/src/main/java/eu/europa/ec/fisheries/schema/exchange/poll/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:poll.exchange.schema.fisheries.ec.europa.eu:v1")
-package eu.europa.ec.fisheries.schema.exchange.poll.v1;
--- model/src/main/java/eu/europa/ec/fisheries/schema/exchange/service/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:service.exchange.schema.fisheries.ec.europa.eu:v1")
-package eu.europa.ec.fisheries.schema.exchange.service.v1;
