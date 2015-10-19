edd2b9aebc49f45a8e998941649996bca391be6c
emhedenma <emhedenma@mycompany.com>
2015-10-19 14:20:26 +0000

    Accidentially removed TextMessage import.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Exchange/APP@5735 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/exchange/service/bean/ExchangeEventServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/exchange/service/bean/ExchangeEventServiceBean.java
@@ -8,6 +8,7 @@ import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import javax.jms.TextMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
