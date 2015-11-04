7346e7f637f28c17080163b0899d454da0d447dc
emlynnjoh <emlynnjoh@mycompany.com>
2015-11-04 09:50:21 +0000

    Missing files. @Review Johanna, Joakim DateUtils in module-model.jar. Remove dependency on commons.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Exchange/APP@6319 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- model/src/main/java/eu/europa/ec/fisheries/uvms/exchange/model/util/DateUtils.java
+++ model/src/main/java/eu/europa/ec/fisheries/uvms/exchange/model/util/DateUtils.java
@@ -7,14 +7,23 @@ package eu.europa.ec.fisheries.uvms.exchange.model.util;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
+
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DateUtils {
-
+	private static Logger LOG = LoggerFactory.getLogger(DateUtils.class);
+    final static String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss Z";
+    final static String DATE_FORMAT = "yyyy-MM-dd";
+	
     public static XMLGregorianCalendar dateToXmlGregorian(Date timestamp) {
         GregorianCalendar cal = new GregorianCalendar();
         cal.setTime(timestamp);
@@ -35,5 +44,16 @@ public class DateUtils {
             return timestamp.toGregorianCalendar().getTime();
         }
         return null;
+    }
+
+    private static Date parseToUTC(String format, String dateString) {
+    	DateTimeFormatter formatter = DateTimeFormat.forPattern(format).withOffsetParsed();
+    	DateTime dateTime = formatter.withZoneUTC().parseDateTime(dateString);
+    	GregorianCalendar cal = dateTime.toGregorianCalendar();
+    	return cal.getTime();
+    }
+    
+    public static Date parseToUTCDateTime(String dateString) {
+        return parseToUTC(DATE_TIME_FORMAT, dateString);
     }
 }
