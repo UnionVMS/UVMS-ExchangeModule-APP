709921be94406b3bdec4ede1099d2b4108379c15
emlynhi <emlynhi@mycompany.com>
2016-01-27 15:23:40 +0000

    [UVMS-0000] Removed pooledConnectionFactory
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Exchange/APP@8032 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- message/src/main/java/eu/europa/ec/fisheries/uvms/exchange/message/producer/bean/JMSConnectorBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/exchange/message/producer/bean/JMSConnectorBean.java
@@ -1,7 +1,6 @@
 package eu.europa.ec.fisheries.uvms.exchange.message.producer.bean;
 
 import eu.europa.ec.fisheries.uvms.exchange.model.constant.ExchangeModelConstants;
-import org.apache.activemq.jms.pool.PooledConnectionFactory;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
@@ -21,17 +20,13 @@ public class JMSConnectorBean {
     @Resource(lookup = ExchangeModelConstants.CONNECTION_FACTORY)
     private ConnectionFactory connectionFactory;
 
-    private PooledConnectionFactory pooledConnectionFactory;
     private Connection connection;
 
     @PostConstruct
     private void connectToQueue() {
         LOG.debug("Open connection to JMS broker");
         try {
-            pooledConnectionFactory = new PooledConnectionFactory();
-            pooledConnectionFactory.setConnectionFactory(connectionFactory);
-            connection = pooledConnectionFactory.createConnection();
-//            connection = connectionFactory.createConnection();
+            connection = connectionFactory.createConnection();
             connection.start();
         } catch (JMSException ex) {
             LOG.error("Error when open connection to JMS broker");
