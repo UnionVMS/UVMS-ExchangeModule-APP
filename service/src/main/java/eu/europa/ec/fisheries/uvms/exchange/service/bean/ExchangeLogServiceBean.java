7bf81de1ebdc67ae4c230608f8597ec935eb2281
ANKOVI <fevamko@yahoo.com>
2018-10-18 16:04:50 +0200

    improvement/add_ad_value_for_incoming_messages

--- service/src/main/java/eu/europa/ec/fisheries/uvms/exchange/service/bean/ExchangeLogServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/exchange/service/bean/ExchangeLogServiceBean.java
@@ -108,6 +108,7 @@ public class ExchangeLogServiceBean implements ExchangeLogService {
         log.setTodt(request.getTodt());
         log.setDf(request.getFluxDataFlow());
         log.setGuid(request.getResponseMessageGuid());
+        log.setAd(request.getAd());
 
         return log(log, request.getUsername());
     }
