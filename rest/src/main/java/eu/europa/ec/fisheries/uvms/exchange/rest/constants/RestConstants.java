3ac775561644d90f0ff14b1f38712ed7f5bc365f
Kasim Gul <kasimgul@gmail.com>
2019-10-28 09:27:27 +0100

    Cors support update

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/exchange/rest/constants/RestConstants.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/exchange/rest/constants/RestConstants.java
@@ -26,5 +26,5 @@ public class RestConstants {
     public static final String ACCESS_CONTROL_ALLOWED_METHODS = "GET, POST, DELETE, PUT, OPTIONS";
 
     public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
-    public static final String ACCESS_CONTROL_ALLOW_HEADERS_ALL = "Content-Type";
+    public static final String ACCESS_CONTROL_ALLOW_HEADERS_ALL = "Content-Type, X-Requested-With, Authorization, Cache-Control";
 }
