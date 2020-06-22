7de83e0055e240a64b2152be17b56f555131c50b
pesjob <peter.sjoberg@havochvatten.se>
2020-06-22 09:04:19 +0200

    Introducing a new plugin in ERS/EFR: * using new JMS_FUNCTION_PROPERTY: EFR_SAVE_REPORT and EFR_REPORT_SAVED * communication: Plugin<>Exchange<>Activity

--- service/src/main/java/eu/europa/ec/fisheries/uvms/exchange/service/constants/ExchangeServiceConstants.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/exchange/service/constants/ExchangeServiceConstants.java
@@ -18,10 +18,11 @@ public class ExchangeServiceConstants {
     public static final String GLOBAL_DB_ACCESS_EXCHANGE_LOG = "java:global/exchange-dbaccess-module/exchange-dbaccess-domain/ExchangeLogModelBean!eu.europa.ec.fisheries.uvms.exchange.model.remote.ExchangeLogModel";
     public static final String GLOBAL_DB_ACCESS_UNSENT_MESSAGE = "java:global/exchange-dbaccess-module/exchange-dbaccess-domain/UnsentModelBean!eu.europa.ec.fisheries.uvms.exchange.model.remote.UnsentModel";
     public static final String GLOBAL_DB_ACCESS_SERVICE_REGISTRY = "java:global/exchange-dbaccess-module/exchange-dbaccess-domain/ServiceRegistryModelBean!eu.europa.ec.fisheries.uvms.exchange.model.remote.ServiceRegistryModel";
-    public static final String FLUX_ACTIVITY_PLUGIN_SERVICE_NAME= "eu.europa.ec.fisheries.uvms.plugins.flux.activity";
+    public static final String FLUX_ACTIVITY_PLUGIN_SERVICE_NAME = "eu.europa.ec.fisheries.uvms.plugins.flux.activity";
     public static final String BELGIAN_ACTIVITY_PLUGIN_SERVICE_NAME = "eu.europa.ec.fisheries.uvms.plugins.belgian.activity";
     public static final String FLUX_SALES_PLUGIN_SERVICE_NAME = "eu.europa.ec.fisheries.uvms.plugins.flux.sales";
     public static final String BELGIAN_AUCTION_SALES_PLUGIN_SERVICE_NAME = "eu.europa.ec.fisheries.uvms.plugins.belgianauction.sales";
     public static final String MDR_PLUGIN_SERVICE_NAME = "eu.europa.ec.fisheries.uvms.plugins.mdr";
     public static final String FLUX_VESSEL_PLUGIN_SERVICE_NAME = "eu.europa.ec.fisheries.uvms.plugins.flux.vessel";
+    public static final String EFR_PLUGIN_SERVICE_NAME = "se.havochvatten.ers.plugin";
 }
