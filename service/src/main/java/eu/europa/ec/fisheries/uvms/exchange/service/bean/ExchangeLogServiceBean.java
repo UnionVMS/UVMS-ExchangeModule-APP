10a64bd33fbbe1f10e2514ffa6d63ee8c4fd027e
nomikosi <42890529+nomikosi@users.noreply.github.com>
2021-05-18 15:17:21 +0300

    [MOVEMENT-RULES-FIXES] fixed guid for exchange view

--- service/src/main/java/eu/europa/ec/fisheries/uvms/exchange/service/bean/ExchangeLogServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/exchange/service/bean/ExchangeLogServiceBean.java
@@ -297,10 +297,6 @@ public class ExchangeLogServiceBean implements ExchangeLogService {
         ExchangeLogWithValidationResults validationFromRules = new ExchangeLogWithValidationResults();
         if (rawMsg.getType() != null){
 
-            if ( TypeRefType.MOVEMENT == rawMsg.getType()){
-                guid = rawMsg.getRefGuid();
-            }
-
             validationFromRules = exchangeToRulesSyncMsgBean.getValidationFromRules(guid, rawMsg.getType(), rawMsg.getDataFlow());
             validationFromRules.setMsg(rawMsg.getRawMsg() != null ? rawMsg.getRawMsg() : StringUtils.EMPTY);
         }
